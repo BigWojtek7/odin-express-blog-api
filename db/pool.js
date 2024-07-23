a69e2708c96422fac8964238b4f1194c6ad0a761
WalterBigZ <150455054+BigWojtek7@users.noreply.github.com>
2024-07-23 20:13:16 +0200

    Hide db URL to env

--- db/pool.js
+++ db/pool.js
@@ -1,5 +1,5 @@
 const { Pool } = require('pg');
 
 module.exports = new Pool({
-  connectionString: 'postgresql://pusia:podate@localhost:5432/blogapi',
+  connectionString: process.env.DATABASE_URL,
 });
